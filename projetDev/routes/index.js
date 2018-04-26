e50fc5fe79471c0e92cbac7dd3236f4f5355b6ed
braxyxx <braxyxx31@gmail.com>
2018-04-26 14:45:20 +0200

    changement dossier projet dev

--- projetDev/routes/index.js
+++ projetDev/routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.ejs');
 });
+
+var SerialPort = require("serialport");
+
+
+var serialPort = new SerialPort("COM7", {
+  baudRate: 9600,
+});
+
+
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log(data);
+  });
+});
+
 
 module.exports = router;
