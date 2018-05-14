228ccaea90479b12682973107a4e4cd5c4d66d24
huntike <logan.lelay@orange.fr>
2018-05-14 16:59:01 +0200

    ajout graphique

--- projetDev/routes/index.js
+++ projetDev/routes/index.js
@@ -6,21 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index.ejs');
 });
 
+router.get('/reglage', function(req, res, next) {
+  res.render('reglage.ejs');
+});
+
+router.get('/statistique', function(req, res, next) {
+  res.render('statistique.ejs');
+});
+
+
 //var SerialPort = require("serialport");
 
 
-// var serialPort = new SerialPort("COM7", {
-//   baudRate: 9600,
-// });
+//var serialPort = new SerialPort("COM7", {
+ // baudRate: 9600,
+//});
 
 
 
-// serialPort.on("open", function () {
-//   console.log('open');
-//   serialPort.on('data', function(data) {
-//     console.log(data);
-//   });
-// });
+//serialPort.on("open", function () {
+  //console.log('open');
+  //serialPort.on('data', function(data) {
+    //console.log(data);
+  //});
+//});
 
 
 module.exports = router;
